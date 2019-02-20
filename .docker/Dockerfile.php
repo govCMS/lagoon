bb9fa2e8d88564ceb7b5bab754f57ba4704a9b29
Joseph Zhao <pandaski@outlook.com.au>
2019-02-20 11:53:29 +1100

    Pin govcms8dev docker images to 1.0.0-beta2

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -1,7 +1,7 @@
 ARG CLI_IMAGE
 FROM ${CLI_IMAGE} as cli
 
-FROM govcms8dev/php:1.0.0-beta1
+FROM govcms8dev/php:1.0.0-beta2
 
 # Temporary override until lagoon PR is available in upstream image.
 # https://github.com/amazeeio/lagoon/issues/787
