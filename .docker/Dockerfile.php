f7fa29b0c4769f35579764dae9c6f18386bf1d1b
Steve Worley <sj.worley88@gmail.com>
2019-06-12 09:52:30 +1000

    Remove the GMP module from PHP.
    - Drupal doesn't require GMP so this will remove the module from the container.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -5,10 +5,6 @@ FROM ${CLI_IMAGE} as cli
 
 FROM amazeeio/php:${PHP_IMAGE_VERSION}-fpm${LAGOON_IMAGE_VERSION_PHP}
 
-RUN apk add gmp gmp-dev \
-    && docker-php-ext-install gmp \
-    && docker-php-ext-configure gmp
-
 RUN apk add --update clamav clamav-libunrar \
     && freshclam
 
