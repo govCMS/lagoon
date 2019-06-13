d8008134524640bf3a9c019565d6c859ea22bff6
Steve <sj.worley88@gmail.com>
2019-06-13 11:10:44 +1000

    Revert removing clamav binaries.
    This should be tackled in another commit so that we can trickle users onto the service and monitor.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -1,3 +1,4 @@
+
 ARG CLI_IMAGE
 ARG LAGOON_IMAGE_VERSION_PHP
 ARG PHP_IMAGE_VERSION
@@ -8,6 +9,9 @@ FROM amazeeio/php:${PHP_IMAGE_VERSION}-fpm${LAGOON_IMAGE_VERSION_PHP}
 RUN apk add gmp gmp-dev \
     && docker-php-ext-install gmp \
     && docker-php-ext-configure gmp
+
+RUN apk add --update clamav clamav-libunrar \
+    && freshclam
 
 COPY --from=cli /app /app
 COPY .docker/sanitize.sh /app/sanitize.sh
