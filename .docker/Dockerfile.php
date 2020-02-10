129475d3cba894e7e7d9aea38663908546fd9782
Toby Bellwood <toby.bellwood@finance.gov.au>
2020-02-11 10:08:30 +1100

    Use dl-cdn instead of direct link

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -5,7 +5,7 @@ FROM ${CLI_IMAGE} as cli
 
 FROM amazeeio/php:${PHP_IMAGE_VERSION}-fpm-${LAGOON_IMAGE_VERSION}
 
-RUN apk add --no-cache --update clamav clamav-libunrar --repository http://dl-3.alpinelinux.org/alpine/edge/main \
+RUN apk add --no-cache --update clamav clamav-libunrar --repository http://dl-cdn.alpinelinux.org/alpine/edge/main/ \
     && freshclam
 
 COPY --from=cli /app /app
