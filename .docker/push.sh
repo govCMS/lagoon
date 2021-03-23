fbdfbb757bfc9cb4775d1da1f76460ac92a89fd1
Steve Worley <sj.worley88@gmail.com>
2021-03-23 13:35:02 +1000

    Add info to print build information.
    Test info.
    
    Update CI.
    
    Before script trial.
    
    Build script.
    
    manual prepare
    
    Without cp.
    
    Simple build.
    
    Simple build.
    
    Matrix for job definitions.
    
    Hash matrix.
    
    Tag variables.
    
    CI variables.
    
    Add commit ref slug.
    
    Matrix for tests.
    
    Start the containers.
    
    Test edge push.
    
    8.x-edge image for testing.
    
    Add push back.
    
    Branches only for edge/latest deploys.
    
    1.x-develop deploy.
    
    Lint CI.
    
    Change version check.
    
    Add logic to push to swap target image.

--- .docker/push.sh
+++ .docker/push.sh
@@ -21,6 +21,14 @@ FILE_EXTENSION_PREFIX=${FILE_EXTENSION_PREFIX:-.docker/Dockerfile.}
 # CLI Image name
 CLI_IMAGE=${DOCKERHUB_NAMESPACE:-govcms8lagoon}/${GOVCMS_CLI_IMAGE_NAME:-govcms8}
 
+if [[ -n $CI_COMMIT_REF_SLUG ]]; then
+  if [[ "$CI_COMMIT_REF_SLUG" =~ 1.x ]]; then
+    IMAGE_TAG_EDGE="8.x-$IMAGE_TAG_EDGE";
+  else
+    IMAGE_TAG_EDGE="9.x-$IMAGE_TAG_EDGE";
+  fi
+fi
+
 for file in $(echo $FILE_EXTENSION_PREFIX"*"); do
     service=${file/$FILE_EXTENSION_PREFIX/}
 
@@ -43,12 +51,12 @@ for file in $(echo $FILE_EXTENSION_PREFIX"*"); do
     # Tag images with 'edge' tag and push.
     echo "==> Tagged and pushed \"$service\" image to $DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE"
     docker tag $DOCKERHUB_NAMESPACE/$service $DOCKER_REGISTRY_HOST$DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE
-    docker push $DOCKER_REGISTRY_HOST$DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE
+    # docker push $DOCKER_REGISTRY_HOST$DOCKERHUB_NAMESPACE/$service:$IMAGE_TAG_EDGE
 
     # Tag images with version tag, if provided, and push.
     if [ "$version_tag" != "" ]; then
       echo "==> Tagging and pushing \"$service\" image to $DOCKERHUB_NAMESPACE/$service:$version_tag"
       docker tag $DOCKERHUB_NAMESPACE/$service $DOCKER_REGISTRY_HOST$DOCKERHUB_NAMESPACE/$service:$version_tag
-      docker push $DOCKER_REGISTRY_HOST$DOCKERHUB_NAMESPACE/$service:$version_tag
+      # docker push $DOCKER_REGISTRY_HOST$DOCKERHUB_NAMESPACE/$service:$version_tag
     fi
 done
