45c971a672f22077e5ea003bcc9bedda6c3a9fd1
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2023-11-15 10:22:26 +1000

    Removed X-XSS-Protection from headers test.

--- .docker/images/nginx/tests/src/SecurityHeadersTest.php
+++ .docker/images/nginx/tests/src/SecurityHeadersTest.php
@@ -10,15 +10,7 @@ use PHPUnit\Framework\TestCase;
 class SecurityHeadersTest extends TestCase {
 
   /**
-   * Ensure that the X-XSS-Protection header is present.
-   */
-  public function testXssProtection() {
-    $headers = \get_curl_headers("/");
-    $this->assertEquals('1; mode=block', $headers['X-XSS-Protection']);
-  }
-
-  /**
-   * Ensure taht the X-Content-Type-Options header is prsent.
+   * Ensure that the X-Content-Type-Options header is prsent.
    */
   public function testContentTypeOptions() {
     $headers = \get_curl_headers("/");
