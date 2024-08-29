743d66936f50c197586cad2e6d6fad0391bc3deb
Steve Worley <sj.worley88@gmail.com>
2024-08-29 14:41:17 +1000

    Update to php8.3

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -2,7 +2,7 @@ ARG CLI_IMAGE
 ARG LAGOON_IMAGE_VERSION
 FROM ${CLI_IMAGE} as cli
 
-FROM uselagoon/php-8.1-fpm:${LAGOON_IMAGE_VERSION}
+FROM uselagoon/php-8.3-fpm:${LAGOON_IMAGE_VERSION}
 
 COPY .docker/images/php/01-govcms.ini /usr/local/etc/php/conf.d/
 COPY --from=cli /app /app
