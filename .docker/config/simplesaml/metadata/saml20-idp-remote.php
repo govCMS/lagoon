3bf136cf676f4eaf933fa2bb2d694dac8653d4c6
Steve Worley <sj.worley88@gmail.com>
2024-11-18 08:48:07 +1000

    Update settings handling.
    - Supports FQDN for IdP parameters
    - Supports boolean values for sign.authnrequest.

--- .docker/config/simplesaml/metadata/saml20-idp-remote.php
+++ .docker/config/simplesaml/metadata/saml20-idp-remote.php
@@ -4,51 +4,71 @@ $idpBaseURL = getenv('SIMPLESAMLPHP_IDP_BASE_URL');
 $idpEntityId = getenv('SIMPLESAMLPHP_IDP_ENTITYID') ?: $idpBaseURL;
 $fallbackBinding = getenv('SIMPLESAMLPHP_IDP_DEFAULT_BINDING');
 
+$bindings = [
+  'SIMPLESAMLPHP_IDP_HTTP_POST_BINDING' => $fallbackBinding,
+  'SIMPLESAMLPHP_IDP_HTTP_REDIRECT_BINDING' => $fallbackBinding,
+  'SIMPLESAMLPHP_IDP_SOAP_BINDING' => $fallbackBinding,
+  'SIMPLESAMLPHP_IDP_HTTP_ARTIFACT' => $fallbackBinding,
+];
+
+$bindings = array_map(function($fallback, $binding) use ($idpBaseURL) {
+  $envVar = getenv($binding);
+  
+  if (empty($envVar)) {
+    // Return the fallback binding if the environment variable is not set.
+    return $idpBaseURL . $fallback;
+  }
+
+  // If the environment variable is a full URL, use it as is. Otherwise,
+  // prepend the base URL.
+  return str_starts_with($envVar, 'http') ? $envVar : $idpBaseURL . $envVar;
+}, $bindings, array_keys($bindings));
+
 $metadata[$idpEntityId] = [
   'entityid' => $idpEntityId,
   'contacts' => [],
   'metadata-set' => 'saml20-idp-remote',
-  'sign.authnrequest' => getenv('SIMPLESAMLPHP_IDP_SIGN_AUTH') ?: FALSE,
+  'sign.authnrequest' => !empty(getenv('SIMPLESAMLPHP_IDP_SIGN_AUTH')),
   'SingleSignOnService' => [
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_POST_BINDING') ?: $fallbackBinding),
+      'Location' => $bindings['SIMPLESAMLPHP_IDP_HTTP_POST_BINDING'],
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_REDIRECT_BINDING') ?: $fallbackBinding),
+      'Location' => $bindings['SIMPLESAMLPHP_IDP_HTTP_REDIRECT_BINDING'],
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:SOAP',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_SOAP_BINDING') ?: $fallbackBinding),
+      'Location' => $bindings['SIMPLESAMLPHP_IDP_SOAP_BINDING'],
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_ARTIFACT') ?: $fallbackBinding),
+      'Location' => $bindings['SIMPLESAMLPHP_IDP_HTTP_ARTIFACT'],
     ],
   ],
   'SingleLogoutService' => [
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_POST_BINDING') ?: $fallbackBinding),
+      'Location' => $bindings['SIMPLESAMLPHP_IDP_HTTP_POST_BINDING'],
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_REDIRECT_BINDING') ?: $fallbackBinding),
+      'Location' => $bindings['SIMPLESAMLPHP_IDP_HTTP_REDIRECT_BINDING'],
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_HTTP_ARTIFACT') ?: $fallbackBinding),
+      'Location' => $bindings['SIMPLESAMLPHP_IDP_HTTP_ARTIFACT'],
     ],
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:SOAP',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_SOAP_BINDING') ?: $fallbackBinding),
+      'Location' => $bindings['SIMPLESAMLPHP_IDP_SOAP_BINDING'],
     ],
   ],
   'ArtifactResolutionService' => [
     [
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:SOAP',
-      'Location' => $idpBaseURL . (getenv('SIMPLESAMLPHP_IDP_SOAP_BINDING') ?: $fallbackBinding),
+      'Location' => $bindings['SIMPLESAMLPHP_IDP_SOAP_BINDING'],
       'index' => 0,
     ],
   ],
