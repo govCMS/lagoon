c7c18055d8ac14bad4fb9f89426cecf731b32576
barbun <roman.barbun@salsa.digital>
2026-03-16 13:21:06 +1100

    [GOVCMS-13360] Disable SAML SLO fallbacks. (#535)
    * Changed Nginx configuration rules to allow serving static assets for SAML auto-post.
    
    * Disabled SP-initiated SLO by default.
    
    * Disabled fallback values for SLO bindings.
    
    * Reverted nginx change.
    
    ---------
    
    Co-authored-by: Roman Barbun <romanbarbun@gmail.com>

--- .docker/config/simplesaml/metadata/saml20-idp-remote.php
+++ .docker/config/simplesaml/metadata/saml20-idp-remote.php
@@ -2,6 +2,7 @@
 
 $idpBaseURL = getenv('SIMPLESAMLPHP_IDP_BASE_URL');
 $idpEntityId = getenv('SIMPLESAMLPHP_IDP_ENTITYID') ?: $idpBaseURL;
+$singleLogOut = getenv('SIMPLESAMLPHP_SP_SLO') ?: false;
 $fallbackBinding = getenv('SIMPLESAMLPHP_IDP_DEFAULT_BINDING');
 
 $bindingKeys = [
@@ -23,7 +24,7 @@ foreach ($bindingKeys as $key) {
     $envVar = getenv($key);
 
     // Special for LOGOUT: fallback to non-logout sibling if present.
-    if (str_contains($key, 'LOGOUT') && empty($envVar)) {
+    if (str_contains($key, 'LOGOUT') && empty($envVar) && $singleLogOut) {
         $nonLogoutKey = str_replace('LOGOUT_', '', $key);
         $envVar = getenv($nonLogoutKey);
     }
