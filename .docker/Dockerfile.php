2b70616850496c3fd621a04818a4798e6440896d
barbun <robarcover@gmail.com>
2026-05-14 11:42:19 +1000

    Added HTTPAV identifier.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -5,6 +5,7 @@ FROM ${CLI_IMAGE} as cli
 FROM uselagoon/php-8.3-fpm:${LAGOON_IMAGE_VERSION}
 
 COPY .docker/images/php/01-govcms.ini /usr/local/etc/php/conf.d/
+COPY .docker/images/php/99-httpav-id.conf /usr/local/etc/php-fpm.d/99-httpav-id.conf
 COPY --from=cli /app /app
 COPY .docker/sanitize.sh /app/sanitize.sh
 
